2ca824fc91f5839b7afb7c176bb3432e8c500db0
Robert Neyfeld <irobens34@gmail.com>
2021-08-11 18:22:53 +0300

    Revert "Add unyson and tgm"
    This reverts commit 3a1b50af8e6f900e07963ae5ba1a3dff458faff6.

--- wp-content/themes/decoblog/functions.php
+++ wp-content/themes/decoblog/functions.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'decoblog_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'header-menu' => esc_html__( 'Header menu', 'decoblog' ),
+				'menu-1' => esc_html__( 'Primary', 'decoblog' ),
 			)
 		);
 
@@ -146,7 +146,7 @@ function decoblog_scripts() {
     wp_enqueue_style( 'decoblog-bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css' );
     wp_enqueue_style( 'decoblog-bootstrap-icons', get_template_directory_uri() . '/assets/css/bootstrap-icons.css' );
     wp_enqueue_style( 'decoblog-fancybox', get_template_directory_uri() . '/assets/css/fancybox.css' );
-    wp_enqueue_style( 'decoblog-styles', get_template_directory_uri() . '/assets/css/styles.css' );
+    wp_enqueue_style( 'decoblog-styles', get_template_directory_uri() . '/assets/css/style.min.css' );
     wp_enqueue_style( 'decoblog-custom', get_template_directory_uri() . '/assets/css/custom.css' );
 
     wp_deregister_script( 'jquery' );
@@ -191,4 +191,3 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-require_once get_template_directory() . '/inc/tgm/tgm.php';
